872d1c2fcee0eaa9d6a4b6d9a9f79c7bd5109e05
Ziyu Wang <odduid@gmail.com>
2016-06-10 17:17:16 +1000

    make specs run in node

--- app/scripts/lib/version.js
+++ app/scripts/lib/version.js
@@ -1,6 +1,6 @@
 const semver = require('semver'),
-  self = require('json!../../../package.json'),
-  webdriver = require('json!selenium-webdriver/package.json');
+  self = require('../../../package.json'),
+  webdriver = require('selenium-webdriver/package.json');
 
 module.exports = semver.parse(self.version);
 module.exports['webdriver-version'] = webdriver.version;

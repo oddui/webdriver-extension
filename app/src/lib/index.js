09192cd12dc79daccf479efaa0495c3fb97f4843
Ziyu Wang <odduid@gmail.com>
2016-09-16 12:00:53 +1000

    export logging

--- app/src/lib/index.js
+++ app/src/lib/index.js
@@ -1,9 +1,11 @@
 const by = require('selenium-webdriver/lib/by');
+const logging = require('selenium-webdriver/lib/logging');
 const until = require('selenium-webdriver/lib/until');
 const version = require('./version');
 const builder = require('./builder');
 
 exports.Builder = builder.Builder;
 exports.By = by.By;
+exports.logging = logging;
 exports.until = until;
 exports.version = version;
--- app/test/spec/index_spec.js
+++ app/test/spec/index_spec.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect,
   index = require('../../src/lib/index');
 
 describe('index', function() {
-  ['version', 'Builder', 'By', 'until'].forEach(function(func) {
+  ['version', 'Builder', 'By', 'until', 'logging'].forEach(function(func) {
     it(`exports ${func}`, function() {
       expect(index[func]).not.to.be.undefined;
     });

5da074c1abcf41db22a3ece951446f07e2f453be
Ziyu Wang <odduid@gmail.com>
2016-09-20 09:18:37 +1000

    run e2e tests in mocha

--- app/test/e2e.js
+++ app/test/e2e.js
@@ -6,56 +6,73 @@ const webdriver = require('../src/lib/index'),
   until = webdriver.until,
   Builder = webdriver.Builder;
 
-if (process.browser) {
-  // only run in browser
 
-  run();
-  run(true);
-}
+describe('e2e', function() {
+  let builder;
 
-/**
- * Run e2e tests
- *
- * @param {boolean} remote Whether to use http webdriver server.
- */
-function run(remote) {
-  webdriver.logging.installConsoleHandler();
-
-  ['webdriver.extension', 'webdriver.http']
-    .forEach(function(name) {
-      webdriver.logging.getLogger(name)
-        .setLevel(webdriver.logging.Level.ALL);
-    });
-
-  // chrome specific options/capabilities
-  let chromeOptions = new chrome.Options()
-    .setMobileEmulation({deviceName: 'Google Nexus 5'});
-
-  let builder = new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(chromeOptions);
-
-  if (remote) {
-    // manually started webdriver server
-    builder.usingServer('http://127.0.0.1:9515');
+  if (!process.browser) {
+    // only run in browser
+    return;
   }
 
-  let driver = builder.build();
+  before(function() {
+    webdriver.logging.installConsoleHandler();
 
-
-  let message = [];
-  driver.call(message.push, message, 'a').then(function() {
-    driver.call(message.push, message, 'b');
-  });
-  driver.call(message.push, message, 'c');
-  driver.call(function() {
-    console.log('message is abc? ' + (message.join('') === 'abc'));
+    ['webdriver.extension', 'webdriver.http'].forEach(function(name) {
+      webdriver.logging.getLogger(name).setLevel(webdriver.logging.Level.ALL);
+    });
   });
 
+  beforeEach(function() {
+    // chrome specific options/capabilities
+    let chromeOptions = new chrome.Options()
+      .setMobileEmulation({deviceName: 'Google Nexus 5'});
 
-  driver.get('http://www.google.com/ncr');
-  driver.findElement(By.name('q')).sendKeys('webdriver');
-  driver.findElement(By.name('btnG')).click();
-  driver.wait(until.titleIs('webdriver - Google Search'), 5000);
-  driver.quit();
-}
+    builder = new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(chromeOptions);
+  });
+
+
+  describe('extension', function() {
+    let driver;
+
+    beforeEach(function() {
+      driver = builder.build();
+    });
+
+    afterEach(function() {
+      return driver.quit();
+    });
+
+    it('should append query to title', function() {
+      driver.get('http://www.google.com/ncr');
+      driver.findElement(By.name('q')).sendKeys('webdriver');
+      driver.findElement(By.name('btnG')).click();
+      return driver.wait(until.titleIs('webdriver - Google Search'), 5000);
+    });
+  });
+
+
+  describe('http', function() {
+    let driver;
+
+    beforeEach(function() {
+      // manually started webdriver server
+      builder.usingServer('http://127.0.0.1:9515');
+      driver = builder.build();
+    });
+
+    afterEach(function() {
+      return driver.quit();
+    });
+
+    it('should append query to title', function() {
+      driver.get('http://www.google.com/ncr');
+      driver.findElement(By.name('q')).sendKeys('webdriver');
+      driver.findElement(By.name('btnG')).click();
+      return driver.wait(until.titleIs('webdriver - Google Search'), 5000);
+    });
+  });
+
+});
--- app/test/index.js
+++ app/test/index.js
@@ -1,4 +1,21 @@
 'use strict';
 
-require('./unit');
-require('./e2e');
+const mocha = require('mocha');
+
+mocha.setup({
+  ui: 'bdd',
+  timeout: 20000
+});
+
+require('./spec/builder_spec.js');
+require('./spec/chrome_spec.js');
+require('./spec/http_spec.js');
+require('./spec/index_spec.js');
+require('./spec/version_spec.js');
+require('./spec/extension/debugger_spec.js');
+require('./spec/extension/index_spec.js');
+require('./spec/extension/session_commands_spec.js');
+
+require('./e2e.js');
+
+mocha.run();
